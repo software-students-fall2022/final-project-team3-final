ff7e265d8255e87372aba08ca6e85785a74e3862
graces <gracezhang89gz@gmail.com>
2022-12-18 01:23:06 -0500

    Edited MongoDB connection for frontend

--- web-app/app.py
+++ web-app/app.py
@@ -3,26 +3,33 @@ from pymongo import MongoClient
 from dotenv import dotenv_values
 import os
 
-# global variables 
+# global variables
 config = dotenv_values(".env")
 app = Flask(__name__)
 
 # connect to the database
-# don't know if we want to change this for next time 
-database = None
-cxn = pymongo.MongoClient("mongodb", 27017)
+# provide MongoDB Atlas URL
+CONNECTION_STRING = "mongodb+srv://user:<root>@cluster0.fgszvaj.mongodb.net/?retryWrites=true&w=majority"
+# create connection
+client = MongoClient(CONNECTION_STRING)
 try:
-	# verify the connection works by pinging the database
-	cxn.admin.command('ping') # The ping command is cheap and does not require auth.
-	#database = cxn['___'] # store a reference to the database
-	print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
-	
+    # verify the connection works by pinging the database
+    # The ping command is cheap and does not require auth.
+    client.admin.command('ping')
+    # database = cxn['___'] # store a reference to the database
+    # if we get here, the connection worked!
+    database = client["recipes"]
+    collection = database["Data"]
+    print(' *', 'Connected to MongoDB!')
+except:
+    print("MongoDB not connected.")
+
+
 @app.route("/")
 def index():
-	return render_template('index.html')
-
+    return render_template('index.html')
 
 
 # run the app
 if __name__ == "__main__":
-	app.run()
+    app.run()
