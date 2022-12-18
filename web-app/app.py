10da7bf9a3e3e173c5f38618130905d0dedc729c
graces <gracezhang89gz@gmail.com>
2022-12-18 01:25:57 -0500

    Resolved branch conflicts

--- web-app/app.py
+++ web-app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, abort, url_for, make_response, flash
-# from pymongo import MongoClient
-# from dotenv import dotenv_values
+from pymongo import MongoClient
+from dotenv import dotenv_values
 import os
 from os.path import dirname, join
 import sys
@@ -29,22 +29,26 @@ try:
 except:
     print("MongoDB not connected.")
 
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
-	if request.method == "POST":
-		#store ingredients user enters into list
-		userInput = request.form['userInput']
-		print(userInput)
-		return redirect('/results', code=302)
-	return render_template('index.html')
+    if request.method == "POST":
+        # store ingredients user enters into list
+        userInput = request.form['userInput']
+        print(userInput)
+        return redirect('/results', code=302)
+    return render_template('index.html')
 
-# display results 
+# display results
+
+
 @app.route('/results')
 def results():
-	recipe = {'img': 'img-link', 'name': 'pizza', 'spices': 'oregano, basil'}
-	recipes = [recipe]
-	#recipes = recipe_logic.get_recipes(userInput) <- for later
-	return render_template('results.html', recipes=recipes)
+    recipe = {'img': 'img-link', 'name': 'pizza', 'spices': 'oregano, basil'}
+    recipes = [recipe]
+    # recipes = recipe_logic.get_recipes(userInput) <- for later
+    return render_template('results.html', recipes=recipes)
+
 
 # run the app
 if __name__ == "__main__":
